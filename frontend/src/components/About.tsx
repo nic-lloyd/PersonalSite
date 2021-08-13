e09918dcdb1f41b4e24e3c36021980292b380a1e
nic-lloyd <61630547+nic-lloyd@users.noreply.github.com>
2021-08-13 13:58:27 -0500

    I dont know whats going on with this thing

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -3,6 +3,7 @@ import {
   IonCard,
   IonCardContent,
   IonCardHeader,
+  IonCardSubtitle,
   IonCardTitle,
   IonCol,
   IonGrid,
@@ -32,38 +33,43 @@ const About: React.FC = () => {
         </IonCardContent>
       </IonCard>
       <IonGrid>
-          <IonRow>
-            <IonCol>
-                <IonCard color='tertiary'>
-                    <IonCardHeader>
-                        <IonCardTitle className="title">
-                            Tech Stack
-                        </IonCardTitle>
-                        <IonCardContent className='img-container'>
-                            <img src='https://raw.githubusercontent.com/get-icon/geticon/fc0f660daee147afb4a56c64e12bde6486b73e39/icons/nodejs-icon.svg' />
-                            <img src='https://raw.githubusercontent.com/get-icon/geticon/fc0f660daee147afb4a56c64e12bde6486b73e39/icons/mysql.svg' />
-                            <img src='https://raw.githubusercontent.com/get-icon/geticon/fc0f660daee147afb4a56c64e12bde6486b73e39/icons/react.svg' />
-                            <img src='https://raw.githubusercontent.com/get-icon/geticon/fc0f660daee147afb4a56c64e12bde6486b73e39/icons/express.svg' />
-                        </IonCardContent>
-                    </IonCardHeader>
-                </IonCard>
-            </IonCol>
-            <IonCol>
-            <IonCard color='tertiary'>
-                    <IonCardHeader>
-                        <IonCardTitle className="title">
-                            Languages
-                        </IonCardTitle>
-                        <IonCardContent className='img-container'>
-                            <img src='https://raw.githubusercontent.com/get-icon/geticon/fc0f660daee147afb4a56c64e12bde6486b73e39/icons/nodejs-icon.svg' />
-                            <img src='https://raw.githubusercontent.com/get-icon/geticon/fc0f660daee147afb4a56c64e12bde6486b73e39/icons/mysql.svg' />
-                            <img src='https://raw.githubusercontent.com/get-icon/geticon/fc0f660daee147afb4a56c64e12bde6486b73e39/icons/react.svg' />
-                            <img src='https://raw.githubusercontent.com/get-icon/geticon/fc0f660daee147afb4a56c64e12bde6486b73e39/icons/express.svg' />
-                        </IonCardContent>
-                    </IonCardHeader>
-                </IonCard>
-            </IonCol>
-          </IonRow>
+        <IonRow>
+          <IonCol>
+            <IonCard color="tertiary">
+              <IonGrid>
+                <IonRow>
+                  <IonCol>
+                    <IonCardTitle className="title">
+                      Reastaurant Swiper
+                    </IonCardTitle>
+                    <IonCardSubtitle>
+                      Ionic React, Node.js, Express.js, MySQL
+                    </IonCardSubtitle>
+                    <IonCardContent>
+                      Fullstack webapp built to help groups of friends decide where to eat.
+                    </IonCardContent>
+                  </IonCol>
+                  <IonCol>
+                    <IonCardContent className="img-container"></IonCardContent>
+                  </IonCol>
+                </IonRow>
+              </IonGrid>
+            </IonCard>
+          </IonCol>
+          <IonCol>
+            <IonCard color="tertiary">
+              <IonCardHeader>
+                <IonCardTitle className="title">Languages</IonCardTitle>
+                <IonCardContent className="img-container">
+                  <img src="https://raw.githubusercontent.com/get-icon/geticon/fc0f660daee147afb4a56c64e12bde6486b73e39/icons/nodejs-icon.svg" />
+                  <img src="https://raw.githubusercontent.com/get-icon/geticon/fc0f660daee147afb4a56c64e12bde6486b73e39/icons/mysql.svg" />
+                  <img src="https://raw.githubusercontent.com/get-icon/geticon/fc0f660daee147afb4a56c64e12bde6486b73e39/icons/react.svg" />
+                  <img src="https://raw.githubusercontent.com/get-icon/geticon/fc0f660daee147afb4a56c64e12bde6486b73e39/icons/express.svg" />
+                </IonCardContent>
+              </IonCardHeader>
+            </IonCard>
+          </IonCol>
+        </IonRow>
       </IonGrid>
     </div>
   );
--- frontend/src/components/TitlePage.tsx
+++ frontend/src/components/TitlePage.tsx
@@ -1,13 +1,23 @@
-import { IonContent, IonGrid, IonRow } from '@ionic/react'
-import './TitlePage.css'
+import { IonCard, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonItem, IonLabel, IonRow } from "@ionic/react";
+import "./TitlePage.css";
 
 const TitlePage: React.FC = () => {
-    return (
-        <IonContent color="primary" className='content'>
-            <h1>Nicolas Lloyd</h1>
-            <h3>aspiring fullstack web developer</h3>
-        </IonContent>
-    )
-}
+  return (
+    <IonContent color="primary" className="content">
+      <IonGrid>
+        <IonRow />
+        <IonRow>
+          <IonCol className='content'>
+            <IonCard color='primary'>
+              <IonCardTitle>Nicolas Lloyd</IonCardTitle>
+              <IonCardSubtitle>aspiring fullstack web developer</IonCardSubtitle>
+            </IonCard>
+          </IonCol>
+        </IonRow>
+        <IonRow />
+      </IonGrid>
+    </IonContent>
+  );
+};
 
-export default TitlePage
+export default TitlePage;
